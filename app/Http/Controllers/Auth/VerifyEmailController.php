c91e80484e475e643f6334f89f54e556bb96bba4
Amira Izham <amiraizham@Amiras-MacBook-Air-2.local>
2025-06-01 19:55:17 +0800

    verify email fixed

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -5,26 +5,26 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
 
 class VerifyEmailController extends Controller
 {
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
-        // Auto-login user by ID if not authenticated
+        // Auto-login user by ID if not logged in
         if (! $request->user()) {
             Auth::loginUsingId($request->route('id'));
         }
 
         if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended('/dashboard?verified=1');
+            return redirect()->intended(route('dashboard') . '?verified=1');
         }
 
         if ($request->user()->markEmailAsVerified()) {
             event(new Verified($request->user()));
         }
 
-        return redirect()->intended('/dashboard?verified=1');
+        return redirect()->intended(route('dashboard') . '?verified=1');
     }
 }
