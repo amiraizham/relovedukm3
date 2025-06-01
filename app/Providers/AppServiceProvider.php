fd689930968b4c7468f0d675e8f2bcf26e0f1d00
Amira Izham <amiraizham@Amiras-MacBook-Air-2.local>
2025-06-01 17:50:27 +0800

    https issue fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
+
+
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 use Inertia\Inertia;
@@ -25,6 +28,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
 
         Inertia::share([
             'auth' => fn() => [
